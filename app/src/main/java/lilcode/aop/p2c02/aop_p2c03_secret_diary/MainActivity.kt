2ede7c4e9818dc6e521c89288b1d6a60ffc3b280
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-29 19:05:15 +0900

    Feat: 비밀번호 변경 버튼 기능 구현 (Kotlin-ktx 사용)

--- app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/MainActivity.kt
@@ -1,11 +1,14 @@
 package lilcode.aop.p2c02.aop_p2c03_secret_diary
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,14 +36,15 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private val openButton: AppCompatButton by lazy{
+    private val openButton: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.openButton)
     }
 
-    private val changePasswordButton: AppCompatButton by lazy{
+    private val changePasswordButton: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.openButton)
     }
 
+    private var changePasswordMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,16 +55,23 @@ class MainActivity : AppCompatActivity() {
         numberPicker2
         numberPicker3
 
-        openButton.setOnClickListener{
+        openButton.setOnClickListener {
+
+            if (changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // 람다 펑션 영역에 해당하는 반환
+            }
+
             // 비밀 번호 데이터 저장에 사용될 수 있는 방식
             // 1. 로컬 db
             // 2. 파일 (shared preferences 등)
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
             // 여기 앱에서만 사용할 것이기 때문에 MODE_PRIVATE 로 사용
 
-            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            val passwordFromUser =
+                "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
 
-            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)){
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
                 // 패스워드 일치
 
                 // TODO 다이어리 페이지 open
@@ -69,11 +80,56 @@ class MainActivity : AppCompatActivity() {
                 AlertDialog.Builder(this)
                     .setTitle("실패!!")
                     .setMessage("비밀번호가 잘못되었습니다.")
-                    .setPositiveButton("확인"){ _, _ -> }
+                    .setPositiveButton("확인") { _, _ -> }
                     .create()
                     .show()
             }
+        }
+        changePasswordButton.setOnClickListener {
 
+            if (changePasswordMode) {
+                // 번호를 저장하는 기능
+                val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+                // old code
+                //val editor = passwordPreferences.edit()
+
+                // new code (kotlin-ktx)
+//                passwordPreferences.edit{
+//                    val passwordFromUser =
+//                        "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+//                    putString("password", passwordFromUser)
+//                    //commit : 저장 될때까지 대기
+//                    //apply : 비동기적으로. 넘어가도 좋다.
+//                    commit()
+//                }
+                passwordPreferences.edit(true){
+                    val passwordFromUser =
+                        "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+                    putString("password", passwordFromUser)
+                }
+            } else {
+                // changePasswodMode 활성화 :: 비밀번호가 맞는지를 체크
+                val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+                // 여기 앱에서만 사용할 것이기 때문에 MODE_PRIVATE 로 사용
+
+                val passwordFromUser =
+                    "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+
+                    changePasswordButton.setBackgroundColor(Color.RED) // 활성화 시 색상
+                } else {
+                    AlertDialog.Builder(this)
+                        .setTitle("실패!!")
+                        .setMessage("비밀번호가 잘못되었습니다.")
+                        .setPositiveButton("확인") { _, _ -> }
+                        .create()
+                        .show()
+                }
+            }
         }
     }
 }
