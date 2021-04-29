1afd904f2c608bfa97647d3b7b5fcb16a619bd95
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-29 22:28:29 +0900

    Feat: Diary Activity 추가 구현

---
+++ app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/DiaryActivity.kt
@@ -0,0 +1,12 @@
+package lilcode.aop.p2c02.aop_p2c03_secret_diary
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+    }
+}
