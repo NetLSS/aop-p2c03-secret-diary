f98fced8f4dd44456311666dfa666a1d662be850
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-29 22:37:23 +0900

    Feat: 다이어리 수정 할 때마다 저장 (빈번)

--- app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/DiaryActivity.kt
+++ app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/DiaryActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
 
 class DiaryActivity : AppCompatActivity() {
 
@@ -20,5 +22,19 @@ class DiaryActivity : AppCompatActivity() {
         val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
 
         diaryEditText.setText(detailPreferences.getString("detail", ""))
+
+        val runnable = Runnable {
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit{
+                putString("detail", diaryEditText.text.toString())
+                apply()
+            }
+        }
+
+        // 수정 할때 마다 저장
+        diaryEditText.addTextChangedListener {
+            detailPreferences.edit {
+                putString("detail", diaryEditText.text.toString() /*it.toString()*/)
+            }
+        }
     }
 }
