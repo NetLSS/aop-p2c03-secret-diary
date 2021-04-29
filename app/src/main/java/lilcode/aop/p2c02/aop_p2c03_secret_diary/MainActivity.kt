83656a155a057b47638e14fc2f8e137d942817c6
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-29 16:59:36 +0900

    Initial commit

---
+++ app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/MainActivity.kt
@@ -0,0 +1,11 @@
+package lilcode.aop.p2c02.aop_p2c03_secret_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
