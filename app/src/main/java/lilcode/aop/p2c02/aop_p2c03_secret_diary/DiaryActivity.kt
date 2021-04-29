bb3514a5329af6a540ee999e0853643aed76264c
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-29 22:32:56 +0900

    Feat: SharedPreferences 로 다이어리 내용 불러오기

--- app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/DiaryActivity.kt
+++ app/src/main/java/lilcode/aop/p2c02/aop_p2c03_secret_diary/DiaryActivity.kt
@@ -1,12 +1,24 @@
 package lilcode.aop.p2c02.aop_p2c03_secret_diary
 
+import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class DiaryActivity : AppCompatActivity() {
+
+    private val _diaryEditText: EditText by lazy{
+        findViewById<EditText>(R.id.diaryEditText)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        val diaryEditText = findViewById<EditText>(R.id.diaryEditText)
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+
+        diaryEditText.setText(detailPreferences.getString("detail", ""))
     }
 }
